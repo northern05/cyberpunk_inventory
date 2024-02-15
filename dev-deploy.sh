6b0e4ebcbfd3850c5a5e9043701fa3d114e274c1
gleb <gleb5shimko@gmail.com>
2024-02-15 19:00:29 +0200

    deploy shel command

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,1 +1,2 @@
-docker compose -f .docker/dev-compose.yml --env-file=.envs/.dev up -d --build
+sudo docker compose -f .docker/dev-compose.yml --env-file=.envs/.dev up -d --build
+sudo docker compose -f .docker/postgres-compose.yml --env-file=.envs/.dev up -d --build
